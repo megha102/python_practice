e42f2a0ff321807fd3fcf0408be7eb2f0e1b9bb9
Megha Gulati <mgulati30@gatech.edu>
2026-06-09 17:59:21 +0530

    day1_python_basics

---
+++ Day1/prac1.py
@@ -0,0 +1,17 @@
+# print("Hello world\nHello world\nHello world")
+
+#Swap two variable values using temp variable
+"""glass1 = "milk"
+glass2 = "juice"
+temp = glass1
+glass1 = glass2
+glass2 = temp
+
+print(glass1)
+print(glass2)"""
+
+
+print("Welcome to the Band Name Generator")
+city_name = input("What's the name of the city you grew up in?")
+pet_name = input("What's your pet name?")
+print(f"Your band name could be {city_name} {pet_name}")
