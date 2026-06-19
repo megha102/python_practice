3a400398a301b859ac95f6e69782d5b1bc33c4a3
Megha Gulati <mgulati30@gatech.edu>
2026-06-19 22:40:12 +0530

    classmethods_and_staticmethods

--- OOPs/Tutorial.py
+++ OOPs/Tutorial.py
@@ -9,7 +9,7 @@ class Employee:
         self.first = first
         self.last = last
         self.pay = pay
-        self.email = first + '.' + 'last' + '@abc.com'
+        self.email = first + '.' + last + '@abc.com'
 
         Employee.num_of_employees+=1
 
@@ -22,6 +22,17 @@ class Employee:
     @classmethod
     def set_raise_amt(cls,amount):
         cls.raise_amount = amount
+
+    @classmethod
+    def from_string(cls,emp_str):
+        first, last, pay = emp_str.split("-")
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
 
 
 #Classes and Instances
@@ -62,8 +73,36 @@ emp2 = Employee('Test','user',60000)
 # print(emp2.raise_amount)
 # print(Employee.raise_amount)
 
-Employee.set_raise_amt(1.05)
+# Employee.set_raise_amt(1.05)
+#
+# print(emp1.raise_amount)
+# print(emp2.raise_amount)
+# print(Employee.raise_amount)
 
-print(emp1.raise_amount)
-print(emp2.raise_amount)
-print(Employee.raise_amount)
+#class methods example
+
+emp_str1 = 'John-Doe-7000'
+emp_str2 = 'Steve-Smith-30000'
+emp_str3 = 'Jane-Doe-90000'
+
+#how to create new employee from string - split
+"""first,last,pay = emp_str1.split("-")
+new_emp1 = Employee(first,last,pay)
+
+print(new_emp1.email)"""
+
+#we don't them to parse the string everytime they want to create new employee
+# so we will use classmethod- from_string()
+
+"""new_emp1 = Employee.from_string(emp_str1)
+
+print(new_emp1.email)"""
+
+#Static methods
+#take a date and return if it's a workday - logical connection on Employee class but does not depend on any instance or methods
+#create a static method in class
+
+import datetime
+my_date = datetime.date(2016,7,11)
+
+print(Employee.is_workday(my_date))
